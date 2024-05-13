af31597324b1b9e33c9300ca34e97501e8bbde10
Divya Mehndiratta <dmrrattaa786@gmail.com>
2024-05-12 20:20:04 -0400

    cover pic modified

--- pages/index.tsx
+++ pages/index.tsx
@@ -299,9 +299,7 @@ const Home: NextPage = ({ user }: any) => {
           property="og:title"
           content={`${profile.firstName} ${profile.lastName}`}
         />
-        {/* <meta property="og:image" content={`https:${assets.profileImage.file.url}`} /> */}
         <meta property="og:locale" content="en_US" />
-        {/* <link rel="icon" href="/favicon.ico" /> */}
         <link
           href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
           rel="stylesheet"
